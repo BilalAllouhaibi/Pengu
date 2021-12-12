d50398616353427cae176b9b7ccce5b9b166efba
BilalAllouhaibi <bilalzmhaa99@gmail.com>
2021-12-12 22:21:39 +0100

    vue app structure, store.js and router.js
    Signed-off-by: BilalAllouhaibi <bilalzmhaa99@gmail.com>

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import {createStore} from 'vuex'
+//import axios from "axios";
+
+const store = createStore({
+    state:{
+
+    },
+    mutations:{
+
+    },
+    actions:{
+        test(){
+            console.log("test")
+        }
+    },
+    getters:{
+
+    }
+});
+export default store
